5bdbb5551fba1fd5a1afb2576a83cceb5d1253a3
Loïc ROY <loic.roy35@orange.fr>
2021-06-14 13:10:54 +0200

    before merge jessica

--- src/main/java/fr/diginamic/IntegrationOpenFoodFacts.java
+++ src/main/java/fr/diginamic/IntegrationOpenFoodFacts.java
@@ -1,13 +1,10 @@
 package fr.diginamic;
 
-import fr.diginamic.entities.Produit;
 import fr.diginamic.service.CsvService;
-import fr.diginamic.service.LengthException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.io.IOException;
 
 public class IntegrationOpenFoodFacts {
     private static final String PATH = "C:/Users/yviqu/Desktop/Projet1_Diginamic/open-food-facts.csv";
